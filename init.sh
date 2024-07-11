e54c46b3a58a181c23437942888ab960e3d6c37f
Ubuntu <josea.ariasp@uqvirtual.edu.co>
2024-07-11 23:30:02 +0000

    Backup automático: 2024-07-11 23:30:01

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Nombre del archivo JAR del servidor
+JAR_FILE="server.jar"
+
+# Matar todos los procesos java que estén ejecutando el servidor
+pkill -f "java -Xmx1024M -Xms1024M -jar $JAR_FILE nogui"
+
+# Ejecutar el servidor en segundo plano
+java -Xmx1024M -Xms1024M -jar $JAR_FILE nogui &
+
